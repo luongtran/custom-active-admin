6fabab6ef7bd48e0b1eb3ead233b51078a1dfc78
Luanht <htluan2605@gmail.com>
2013-09-16 18:21:33 +0700

    create postion

--- app/models/packet.rb
+++ app/models/packet.rb
@@ -1,4 +1,9 @@
 class Packet < ActiveRecord::Base
-  attr_accessible :name, :description, :parent_id ,:position
+  attr_accessible :name, :description, :parent_id, :position
   has_many :children, :class_name => "Packet", :foreign_key => "parent_id"
+  after_create :auto_position
+
+  def auto_position
+    self.position = self.id
+  end
 end
