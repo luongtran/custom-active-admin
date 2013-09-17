b3bc3d8577f5a68e88ddacfcc2838f22c670c246
Luanht <htluan2605@gmail.com>
2013-09-17 11:39:22 +0700

    fix pos

--- app/models/packet.rb
+++ app/models/packet.rb
@@ -1,9 +1,11 @@
 class Packet < ActiveRecord::Base
   attr_accessible :name, :description, :parent_id, :position
   has_many :children, :class_name => "Packet", :foreign_key => "parent_id"
-  after_create :auto_position
+  after_save :auto_position
+
+  private
 
   def auto_position
-    self.position = self.id
+    update_column('position', self.id)
   end
 end
